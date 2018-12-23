91f572a039d7fbb61287276072d52b5a250e9a91
n4ks <30715874+n4ks@users.noreply.github.com>
2018-12-23 11:47:58 +0300

    повесил JS на карту

--- js/script.js
+++ js/script.js
@@ -40,3 +40,27 @@ window.addEventListener("keydown", function (evt) {
       }
     }
   });
+
+  var mapLink = document.querySelector(".map-link");
+  
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".modal-close");
+
+  mapLink.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function(evt){
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
