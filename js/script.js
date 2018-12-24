d0b20f35febbfb5aa1a071aac5a62b3dc12b25ad
n4ks <30715874+n4ks@users.noreply.github.com>
2018-12-24 16:52:18 +0300

    доделать проверку на невалидное значение на форме

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ var feedbackPopup = document.querySelector(".modal-feedback");
 
 var feedbackClose = feedbackPopup.querySelector(".modal-close");
 
-var nameField = feedbackPopup.querySelector("[name=name]");
+var name = feedbackPopup.querySelector("[name=name]");
+
 var email = feedbackPopup.querySelector("[name=email]");
+
 var feedbackText = feedbackPopup.querySelector("[name=feedback-text]");
 
 var form = feedbackPopup.querySelector("form");
@@ -13,7 +15,7 @@ var form = feedbackPopup.querySelector("form");
 feedbackButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.add("modal-show");
-  nameField.focus();
+  name.focus();
 });
 
 feedbackClose.addEventListener("click", function (evt) {
@@ -23,45 +25,45 @@ feedbackClose.addEventListener("click", function (evt) {
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!nameField.value || !email.value || !feedbackText.value) {
-      feedbackPopup.classList.remove("modal-error");
-      feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+  if (!name.value || !email.value || !feedbackText.value) {
+    feedbackPopup.classList.remove("modal-error");
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
     feedbackPopup.classList.add("modal-error");
   }
   evt.preventDefault();
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (feedbackPopup.classList.contains("modal-show")) {
-        feedbackPopup.classList.remove("modal-show");
-        feedbackPopup.classList.remove("modal-error");
-      }
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (feedbackPopup.classList.contains("modal-show")) {
+      feedbackPopup.classList.remove("modal-show");
+      feedbackPopup.classList.remove("modal-error");
     }
-  });
+  }
+});
 
-  var mapLink = document.querySelector(".map-link");
-  
-  var mapPopup = document.querySelector(".modal-map");
+var mapLink = document.querySelector(".map-link");
+
+var mapPopup = document.querySelector(".modal-map");
 
 //   var mapClose = mapPopup.querySelector(".modal-close");
 
-  mapLink.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      mapPopup.classList.add("modal-show");
-  });
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
 
-  mapClose.addEventListener("click", function(evt){
-      evt.preventDefault();
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
-  });
-
-  window.addEventListener("keydown", function (evt) {
-    evt.preventDefault();
-    if (evt.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-show")) {
-        mapPopup.classList.remove("modal-show");
-      }
     }
-  });
+  }
+});
