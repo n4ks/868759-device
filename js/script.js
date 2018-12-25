8e677c30acbecd9426ad801d8aeceb96b75e7104
n4ks <30715874+n4ks@users.noreply.github.com>
2018-12-25 19:31:18 +0300

    добавил цвет поля при невалидном значении

--- js/script.js
+++ js/script.js
@@ -1,35 +1,67 @@
 var feedbackButton = document.querySelector(".feedback-button");
-
 var feedbackPopup = document.querySelector(".modal-feedback");
-
 var feedbackClose = feedbackPopup.querySelector(".modal-close");
 
-var name = feedbackPopup.querySelector("[name=name]");
-
+var userName = feedbackPopup.querySelector("[name=user-name]");
 var email = feedbackPopup.querySelector("[name=email]");
-
 var feedbackText = feedbackPopup.querySelector("[name=feedback-text]");
-
 var form = feedbackPopup.querySelector("form");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("userName");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 feedbackButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.add("modal-show");
+  if (storage) {
+    userName.value = storage;
+    email.focus();
+  } else {
+    userName.focus();
+  }
 });
 
 feedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.remove("modal-show");
   feedbackPopup.classList.remove("modal-error");
+  userName.style.backgroundColor = "";
+  email.style.backgroundColor = "";
+  feedbackText.style.backgroundColor = "";
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value || !feedbackText.value) {
+  if (!userName.value || !email.value || !feedbackText.value) {
+    evt.preventDefault();
     feedbackPopup.classList.remove("modal-error");
     feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
     feedbackPopup.classList.add("modal-error");
+    if (!userName.value) {
+      userName.style.backgroundColor = "#edb5b5";
+    } else {
+      userName.style.backgroundColor = "";
+    }
+    if (!email.value) {
+      email.style.backgroundColor = "#edb5b5";
+    } else {
+      email.style.backgroundColor = "";
+    }
+    if (!feedbackText.value) {
+      feedbackText.style.background = "#edb5b5";
+    } else {
+      feedbackText.style.backgroundColor = "";
+    }
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("userName", userName.value)
+    }
   }
-  evt.preventDefault();
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -38,14 +70,15 @@ window.addEventListener("keydown", function (evt) {
     if (feedbackPopup.classList.contains("modal-show")) {
       feedbackPopup.classList.remove("modal-show");
       feedbackPopup.classList.remove("modal-error");
+      userName.style.backgroundColor = "";
+      email.style.backgroundColor = "";
+      feedbackText.style.backgroundColor = "";
     }
   }
 });
 
 var mapLink = document.querySelector(".map-link");
-
 var mapPopup = document.querySelector(".modal-map");
-
 var mapClose = mapPopup.querySelector(".modal-close");
 
 mapLink.addEventListener("click", function (evt) {
